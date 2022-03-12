9198f9d4c13fd6a0188232e25509920f1e207837
jiyang <yang.ji@thoughtworks.com>
2022-03-12 15:13:30 +0800

    add controller

---
+++ src/test/java/com/example/githubactionsjavatest/HelloControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.githubactionsjavatest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+
+  @Test
+  void test(){
+    assertTrue(true);
+  }
+}
