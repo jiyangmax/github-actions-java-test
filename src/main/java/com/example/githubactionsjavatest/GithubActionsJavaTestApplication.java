7e08e6e5e5896cd8a9ed49f2f8e07218ede81110
jiyang <yang.ji@thoughtworks.com>
2022-03-12 14:08:24 +0800

    init repo

---
+++ src/main/java/com/example/githubactionsjavatest/GithubActionsJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsJavaTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GithubActionsJavaTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/githubactionsjavatest/GithubActionsJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsJavaTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
